3ddbff95af88423ff583411505f06d70c39e4df9
Mavic-ux <62545099+Mavic-ux@users.noreply.github.com>
2020-04-15 21:30:23 +0300

    Update pdp.c

--- pdp.c
+++ pdp.c
@@ -34,4 +34,32 @@ void b_write(Adress a, byte b)
         mem[a - 1] |= (word)(b << 8);
     }
 }
+void trace (const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
 
+}
+void load_file(const char* filename)
+{
+    FILE * p= NULL;
+    p = fopen(filename, "rb");
+    Adress a = 0;
+    word num = 0;
+    byte b = 0;
+    if (p == NULL)
+    {
+        perror(filename);
+        printf("File opening error. \n");
+        exit(1);
+    }
+   /*while(fscanf(p, "%x%x", &a, &num) ....)
+    {
+       ... 
+    
+    }/*
+
+    fclose(p);
+}
