b5ef6c0fb42af63be979bd83ea177b1c162a8d7c
Mavic-ux <62545099+Mavic-ux@users.noreply.github.com>
2020-04-15 21:32:41 +0300

    Update pdp.h

--- pdp.h
+++ pdp.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
-
+#include <stdarg.h>
 
 #define MEMSIZE 64*1024
 
@@ -15,10 +15,17 @@ typedef word Adress;
 
 word mem[MEMSIZE];
 
+word reg[8];
+#define pc reg[7]
+
 void b_write(Adress, byte);
 byte b_read(Adress);
 void w_write(Adress, word);
 word w_read(Adress);
+
+void trace (const char *  , ...);
+
+void load_file(const char* file);
 
 
 #endif 
