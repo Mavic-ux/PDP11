d7b4166287918adcd34be9fbd752b2b89f7bdb02
Mavic-ux <62545099+Mavic-ux@users.noreply.github.com>
2020-04-19 14:57:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,103 +3,20 @@
 
 
 
-void w_write(Adress a, word w)
+int main(int argc, char * argv[])
 {
-    if(a < 8)
-        reg[a] = w;
-
-    else
+    if ( argc == 1)
     {
-        mem[a + 1] = (byte)(w >> 8);
-        mem[a] = (byte)w;
-       
+        trace("Usage: ./a.out [listening file]: \n");
     }
-
-
-}
-
-word w_read(Adress a)
-{
-    word w = 0;
-
-    if(a < 8)
-        w = reg[a];
-    else
+    else 
     {
-        w = ((word)mem[a + 1]) << 8;
-        w |= (word)mem[a];
-    }
-    
-    return w;
-
-
+        load_file(argv[1]);
+        trace("File %s opened\n", argv[1]);
+        run();
+    }  
+     
+    return 0;
 }
 
-byte b_read(Adress a)
-{
-    byte b;
-    if (a % 2 == 0)
-        b = mem[a] & 0xFF;
-    else
-    {
-        word w = mem[a - 1];
-        b = w >> 8 & 0xFF;
-    }
-    
-    return b;
-}
-
-void b_write(Adress a, byte b)
-{
-    if (a % 2 == 0)
-        mem[a] = (word)b;
-    else
-    {
-        mem[a - 1] |= (word)(b << 8);
-    }
-}
-
-void trace (const char * fmt, ...)
-{
-    va_list ap;
-    va_start(ap, fmt);
-    vprintf(fmt, ap);
-    va_end(ap);
-
-}
-
-void load_file(const char* filename)
-{
-    FILE* p;
-    p = fopen(filename, "rb");
-
-    Adress adr = 01000;
-    unsigned int n = 0;
-    byte b = 0;
-
-    while(fscanf(p, "%04x%04x", &adr, &n) == 2)
-    {
-        for (unsigned int i = 0; i < n; i++)
-        {
-            fscanf(p, "%02hhx ", &b);
-            b_write(adr, b);
-            adr++;
-        }
-
-    }
-
-    fclose(p);
-}
-
-
-
-void print_reg()
-{
-    for(int i = 0; i < 8; i++)
-    {
-        trace("R%d:%06o ", i, reg[i]);
-    }
-    trace("\n");
-   
-}
 
