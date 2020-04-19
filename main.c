22a327a9554da045f89abd4a265898d8b73d6025
Mavic-ux <62545099+Mavic-ux@users.noreply.github.com>
2020-04-19 14:56:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,34 +3,103 @@
 
 
 
-/*..void test() {
-    //2 bytes
-    word w1 = 0xcb0a;
-    w_write(a1, w1);
-
-    word b0res = b_read(a1);
-
-    word b1res = b_read(a1 + 1);
-
-    printf("ww/br \t %04x = %02hhx%02hhx\n", w1, b1res, b0res);
-   
-    assert(b0 == b0res);
-    assert(b1 == b1res);
-}*/
-
-int main(int argc, char * argv[])
+void w_write(Adress a, word w)
 {
-    if ( argc == 1)
+    if(a < 8)
+        reg[a] = w;
+
+    else
     {
-        printf("User session:\n");
+        mem[a + 1] = (byte)(w >> 8);
+        mem[a] = (byte)w;
+       
     }
-    else 
+
+
+}
+
+word w_read(Adress a)
+{
+    word w = 0;
+
+    if(a < 8)
+        w = reg[a];
+    else
     {
-        load_file(argv[1]);
-        printf(" File %s opened\n", argv[1]);
-    }        
-    return 0;
+        w = ((word)mem[a + 1]) << 8;
+        w |= (word)mem[a];
+    }
+    
+    return w;
+
+
+}
+
+byte b_read(Adress a)
+{
+    byte b;
+    if (a % 2 == 0)
+        b = mem[a] & 0xFF;
+    else
+    {
+        word w = mem[a - 1];
+        b = w >> 8 & 0xFF;
+    }
+    
+    return b;
+}
+
+void b_write(Adress a, byte b)
+{
+    if (a % 2 == 0)
+        mem[a] = (word)b;
+    else
+    {
+        mem[a - 1] |= (word)(b << 8);
+    }
+}
+
+void trace (const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+
+}
+
+void load_file(const char* filename)
+{
+    FILE* p;
+    p = fopen(filename, "rb");
+
+    Adress adr = 01000;
+    unsigned int n = 0;
+    byte b = 0;
+
+    while(fscanf(p, "%04x%04x", &adr, &n) == 2)
+    {
+        for (unsigned int i = 0; i < n; i++)
+        {
+            fscanf(p, "%02hhx ", &b);
+            b_write(adr, b);
+            adr++;
+        }
+
+    }
+
+    fclose(p);
 }
 
 
+
+void print_reg()
+{
+    for(int i = 0; i < 8; i++)
+    {
+        trace("R%d:%06o ", i, reg[i]);
+    }
+    trace("\n");
+   
+}
 
