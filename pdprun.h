c0694730df0b0124fb02995ae2d91ce6bd3fa2c1
Mavic-ux <62545099+Mavic-ux@users.noreply.github.com>
2020-05-04 23:25:26 +0300

    CLR + SOB

--- pdprun.h
+++ pdprun.h
@@ -1,4 +1,3 @@
-#ifndef pdprun_h
 #define pdprun_h
 
 typedef struct
@@ -15,14 +14,22 @@ enum PARAMS_MASK {
     NO_PARAMS,
     HAS_DD,
     HAS_SS,
+    HAS_N,
+    HAS_NN,
+    HAS_R,
+    HAS_XX,
+    HAS_B,
 
 };
 
 
 Command cmd[] = {
-    {0170000, 0010000, "mov", do_MOV, HAS_SS | HAS_DD},
-    {0170000, 0060000, "add", do_ADD, HAS_SS | HAS_DD},
-    {0177777, 0000000, "halt", do_HALT, NO_PARAMS},
+    {0070000, 0010000, "MOV", do_MOV, HAS_B | HAS_SS | HAS_DD},
+    {0170000, 0060000, "ADD", do_ADD, HAS_SS | HAS_DD},
+    {0177777, 0000000, "HALT", do_HALT, NO_PARAMS},
+    {0077700, 0005000, "CLR", do_CLR, HAS_DD},
+    {0177000, 0077000, "SOB", do_SOB, HAS_NN},
+ 
 };
 
 typedef struct {
